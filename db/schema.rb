8536462095500a838ba067f8993ec9369c12b6e0
Chris Allbritton <cla.inbox@gmail.com>
2020-08-20 16:48:20 -0400

    Update index_spec to create passing test for can see pets

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_132353) do
+ActiveRecord::Schema.define(version: 2020_08_20_201527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "pets", force: :cascade do |t|
-    t.binary "image"
+    t.string "image"
     t.string "name"
     t.integer "approximate_age"
     t.string "sex"
