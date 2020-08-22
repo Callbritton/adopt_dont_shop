d143861e0ce4c143d004a8d3be5119a09f794424
Chris Allbritton <cla.inbox@gmail.com>
2020-08-22 12:11:56 -0400

    Update edit_spec and new_spec tests to be more robust and pass

--- spec/features/shelters/edit_spec.rb
+++ spec/features/shelters/edit_spec.rb
@@ -1,27 +1,27 @@
 require "rails_helper"
 
 RSpec.describe "As a visitor When I visit a shelters page" do
-  it "I can click Update Shelter to update a shelter" do
+  it "I can update a shelter" do
     shelter_1 = Shelter.create(
-      name: "Denver Animal Shelter",
-      address: "1241 W Bayaud Ave",
+      name: "Waylon's Animal Shelter",
+      address: "2020 Whirlwind DR",
       city: "Denver",
       state: "CO",
-      zip: "80223"
+      zip: "80014"
     )
     visit "/shelters/#{shelter_1.id}"
     click_link "Update Shelter"
 
     expect(page).to have_current_path "/shelters/#{shelter_1.id}/edit"
 
-    fill_in "Name", with: "New Denver Animal Shelter"
-    fill_in "Address", with: "1241 W Bayaud Ave"
+    fill_in "Name", with: "Willy's Animal House"
+    fill_in "Address", with: "4467 Dog House Ln"
     fill_in "City", with: "Denver"
     fill_in "State", with: "CO"
-    fill_in "Zip", with: "80223"
+    fill_in "Zip", with: "88976"
     click_on "Submit Changes"
 
     expect(page).to have_current_path "/shelters/#{shelter_1.id}"
-    expect(page).to have_content ('New Denver Animal Shelter')
+    expect(page).to have_content ("Willy's Animal House")
   end
-end 
+end
